fa274cba2a631450331ef564eb6aa3306db5197b
Sergio <cotopaco123@hotmail.es>
2025-11-15 15:29:58 -0500

    feat: add CacheConstants with token keys

---
+++ src/Constants/CacheConstants.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Cotopaco\Factus\Constants;
+
+class CacheConstants
+{
+    const string ACCESS_TOKEN = 'factus:access_token';
+    const string REFRESH_ACCESS_TOKEN = 'factus:refresh_access_token';
+}
