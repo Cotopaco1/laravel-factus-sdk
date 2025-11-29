de8df3bf617265f434ee39ee32ab9cc8626c4548
Sergio <cotopaco123@hotmail.es>
2025-11-29 15:55:55 -0500

    feat: add `fromArray` and `manyFromArray` methods to InvoiceItem DTO for easier instantiation from arrays

--- src/DTO/InvoiceItem.php
+++ src/DTO/InvoiceItem.php
@@ -5,21 +5,23 @@ namespace Cotopaco\Factus\DTO;
 class InvoiceItem
 {
     public function __construct(
-        public string $codeReference,
-        public string $name,
-        public int $quantity,
-        public float $discountRate,
-        public float $price,
-        public float $taxRate,
-        public int $unitMeasureId,
-        public int $standardCodeId,
-        public int $isExclude,
-        public int $tributeId,
-        public ?int $schemeId = null,
+        public string  $codeReference,
+        public string  $name,
+        public int     $quantity,
+        public float   $discountRate,
+        public float   $price,
+        public float   $taxRate,
+        public int     $unitMeasureId,
+        public int     $standardCodeId,
+        public int     $isExclude,
+        public int     $tributeId,
+        public ?int    $schemeId = null,
         public ?string $note = null,
-        public ?array $withholdingTaxes = null,
-        public ?array $mandate = null,
-    ) {}
+        public ?array  $withholdingTaxes = null,
+        public ?array  $mandate = null,
+    )
+    {
+    }
 
     public function toArray(): array
     {
@@ -39,5 +41,40 @@ class InvoiceItem
             'withholding_taxes' => $this->withholdingTaxes,
             'mandate' => $this->mandate,
         ], fn ($value) => $value !== null);
+    }
+
+    /**
+     * Create DTO from array
+     * @param array $data
+     * @return self
+     */
+    public static function fromArray(array $data): self
+    {
+        return new self(
+            codeReference: $data['code_reference'],
+            name: $data['name'],
+            quantity: $data['quantity'],
+            discountRate: $data['discount_rate'],
+            price: $data['price'],
+            taxRate: $data['tax_rate'],
+            unitMeasureId: $data['unit_measure_id'],
+            standardCodeId: $data['standard_code_id'],
+            isExclude: $data['is_excluded'],
+            tributeId: $data['tribute_id'],
+            schemeId: $data['scheme_id'] ?? null,
+            note: $data['note'] ?? null,
+            withholdingTaxes: $data['withholding_taxes'] ?? null,
+            mandate: $data['mandate'] ?? null,
+        );
+    }
+
+    /**
+     * Create many DTO from array
+     * @param array $data
+     * @return self[] List of InvoiceItems
+     */
+    public static function manyFromArray(array $data): array
+    {
+        return array_map(fn(array $item) => self::fromArray($item), $data);
     }
 }
