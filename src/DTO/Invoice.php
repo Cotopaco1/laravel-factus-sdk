3f6c03ea80943b013aff986cd37ffff5153a02be
Sergio <cotopaco123@hotmail.es>
2025-11-29 15:56:09 -0500

    feat: add `fromArray` method to Invoice DTO for instantiation from arrays

--- src/DTO/Invoice.php
+++ src/DTO/Invoice.php
@@ -6,23 +6,25 @@ class Invoice
 {
     public function __construct(
         /** @var array<InvoiceItem> */
-        public array $items,
+        public array    $items,
         public Customer $customer,
-        public string $referenceCode,
-        public int $paymentForm = 1,
-        public int $paymentMethodCode = 10,
-        public string $operationType = '10',
-        public bool $sendEmail = true,
-        public ?int $numberingRangeId = null,
-        public ?string $document = null,
-        public ?string $observation = null,
-        public ?string $paymentDueDate = null,
-        public ?array $orderReference = null,
-        public ?array $relatedDocuments = null,
-        public ?array $billingPeriod = null,
-        public ?array $establishment = null,
-        public ?array $allowanceCharges = null,
-    ) {}
+        public string   $referenceCode,
+        public int      $paymentForm = 1,
+        public int      $paymentMethodCode = 10,
+        public string   $operationType = '10',
+        public bool     $sendEmail = true,
+        public ?int     $numberingRangeId = null,
+        public ?string  $document = null,
+        public ?string  $observation = null,
+        public ?string  $paymentDueDate = null,
+        public ?array   $orderReference = null,
+        public ?array   $relatedDocuments = null,
+        public ?array   $billingPeriod = null,
+        public ?array   $establishment = null,
+        public ?array   $allowanceCharges = null,
+    )
+    {
+    }
 
     public function toArray(): array
     {
@@ -44,5 +46,32 @@ class Invoice
             'items' => array_map(fn (InvoiceItem $item) => $item->toArray(), $this->items),
             'allowance_charges' => $this->allowanceCharges,
         ], fn ($value) => $value !== null);
+    }
+
+    /**
+     * Create DTO from array
+     * @param array $data
+     * @return self
+     */
+    public static function fromArray(array $data): self
+    {
+        return new self(
+            items: InvoiceItem::manyFromArray($data['items']),
+            customer: Customer::fromArray($data['customer']),
+            referenceCode: $data['reference_code'],
+            paymentForm: $data['payment_form'] ?? 1,
+            paymentMethodCode: $data['payment_method_code'] ?? 10,
+            operationType: $data['operation_type'] ?? '10',
+            sendEmail: $data['send_email'] ?? true,
+            numberingRangeId: $data['numbering_range_id'] ?? null,
+            document: $data['document'] ?? null,
+            observation: $data['observation'] ?? null,
+            paymentDueDate: $data['payment_due_date'] ?? null,
+            orderReference: $data['order_reference'] ?? null,
+            relatedDocuments: $data['related_documents'] ?? null,
+            billingPeriod: $data['billing_period'] ?? null,
+            establishment: $data['establishment'] ?? null,
+            allowanceCharges: $data['allowance_charges'] ?? null,
+        );
     }
 }
