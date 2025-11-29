2a767c1c814d85afc35c68deec5bbba4eb92cf4f
Sergio <cotopaco123@hotmail.es>
2025-11-29 15:56:33 -0500

    feat: add base `HttpResponse` abstract class with status and message parsing

---
+++ src/Http/HttpResponse.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Cotopaco\Factus\Http;
+
+/* Base HttpResponse */
+abstract class HttpResponse
+{
+    public int $statusCode;
+    public string $status;
+    public string $message;
+
+    public array $rawData;
+
+    public function __construct(public \Illuminate\Http\Client\Response $request)
+    {
+        $this->statusCode = $this->request->getStatusCode();
+        $data = $this->request->json();
+        $this->rawData = $data;
+        $this->status = $data['status'] ?? '';
+        $this->message = $data['message'] ?? '';
+
+    }
+
+    /**
+     * Represents of response in array
+     * */
+    public function toArray(){}
+
+}
